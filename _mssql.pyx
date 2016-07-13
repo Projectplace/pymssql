0a2678a6e5dd715c5fefef0dbce8c09fa64bcaec
Stefan Lundström <stefanl@projectplace.com>
2016-07-13 07:35:26 +0200

    Revert "Fix encoding issue in query serialization"
    This change causes pymssql to update the input parameters and the result is very odd. We havent taken the change into use when it was made, so i'm reverting it for now.
    
    This reverts commit 968717c277f1c6dcf59a2ee540fcfdc106e658d9.

--- _mssql.pyx
+++ _mssql.pyx
@@ -196,7 +196,11 @@ def set_wait_callback(a_callable):
 DEF NUMERIC_BUF_SZ = 45
 
 cdef bytes ensure_bytes(s, encoding='utf-8'):
-    return unicode(s).encode(encoding)
+    try:
+        decoded = s.decode(encoding)
+        return decoded.encode(encoding)
+    except AttributeError:
+        return s.encode(encoding)
 
 cdef void log(char * message, ...):
     if PYMSSQL_DEBUG == 1:
